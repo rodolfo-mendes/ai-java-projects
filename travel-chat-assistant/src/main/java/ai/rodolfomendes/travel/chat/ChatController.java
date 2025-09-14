554bd7129ffddae8eccf88e95923fcce00f052c7
rodolfo.mendes <rodolfo.m.s.mendes@gmail.com>
2025-09-14 16:35:53 -0300

    [travel-chat-assistant] Configuring Langchain4j and integrating ChatModel to controller

--- travel-chat-assistant/src/main/java/ai/rodolfomendes/travel/chat/ChatController.java
+++ travel-chat-assistant/src/main/java/ai/rodolfomendes/travel/chat/ChatController.java
@@ -1,10 +1,19 @@
 package ai.rodolfomendes.travel.chat;
 
+import dev.langchain4j.model.chat.ChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
+    private ChatModel chatModel;
+
+    @Autowired
+    public ChatController(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
     @GetMapping
     @RequestMapping("/hello")
     public String hello() {
@@ -16,10 +25,15 @@ public class ChatController {
         var template = """
             Your message:
             {{userMessage}}
+            
             Response:
-            Hello! This is your Travel Chat Assistant! How can I help you?
+            {{response}}
             """;
 
-        return template.replace("{{userMessage}}", message);
+        var response = chatModel.chat(message);
+
+        return template
+            .replace("{{userMessage}}", message)
+            .replace("{{response}}", response);
     }
 }
