3fc4790aaeb2a9e86bb27adb4505ce4b95d6fd19
rodolfo.mendes <rodolfo.m.s.mendes@gmail.com>
2025-09-14 10:12:51 -0300

    [travel-chat-assistant] Initial commit

---
+++ travel-chat-assistant/src/main/java/ai/rodolfomendes/travel/TravelChatAssistantApplication.java
@@ -0,0 +1,13 @@
+package ai.rodolfomendes.travel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelChatAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelChatAssistantApplication.class, args);
+	}
+
+}
