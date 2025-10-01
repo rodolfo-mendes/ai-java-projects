57632119a2ef4fd7fe1e8fa8aa3ec48254b14a0d
rodolfo.mendes <rodolfo.m.s.mendes@gmail.com>
2025-09-30 22:37:44 -0300

    [spring-boot-chat] Creating a simple Spring Boot chat application

---
+++ spring-boot-chat/src/test/java/ai/rodolfomendes/spring_boot_chat/SpringBootChatApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.rodolfomendes.spring_boot_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
